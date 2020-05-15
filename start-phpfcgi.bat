5f77e5048861330ad9834e6614c9a120616714ed
ustoopia <ustoopia@gmail.com>
2020-05-15 02:27:19 +0200

    Update start-phpfcgi.bat

--- start-phpfcgi.bat
+++ start-phpfcgi.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 ECHO Starting PHP FastCGI...
 set PATH='C:\Server\Nginx1.17\PHP';%PATH%
-"C:\Server\Nginx1.17\extras\RunHiddenConsole.exe" "C:\Server\Nginx1.17\PHP\php-cgi.exe" -b 127.0.0.1:9000
+"extras\RunHiddenConsole.exe" "PHP\php-cgi.exe" -b 127.0.0.1:9000
